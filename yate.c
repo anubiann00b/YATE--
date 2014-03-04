27a469ba91a958d5d1c3932a0230ddecce713b73
Shreyas <skraman1999@gmail.com>
2014-03-04 09:14:10 -0800

    Removed more unneeded bloat.

--- yate.c
+++ yate.c
@@ -9,14 +9,13 @@ void end(int signal){
 int main(int argc, char *argv[]){
 	if (argc != 2)
 		return -1;
-	fp = fopen(argv[1], "a");
+	fp = fopen(argv[1],"a");
 	if (fp == NULL)
 		return -1;
 	signal(SIGINT, end);
 	char c;
 	while(1){
 		c = getchar();
-		fprintf(fp, "%c", c);
+		fprintf(fp,"%c",c);
 	}
 }
-
