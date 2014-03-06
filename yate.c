588ab694fc12ad9b660563d35b2cbbe5845c3086
Shreyas <skraman1999@gmail.com>
2014-03-05 17:09:03 -0800

    Spaces waste space.

--- yate.c
+++ yate.c
@@ -6,15 +6,15 @@ void end(int signal){
 	fclose(f);
 	exit(0);
 }
-int main(int n, char *a[]){
-	if (n != 2)
+int main(int n,char *a[]){
+	if (n!=2)
 		return -1;
-	if ((f = fopen(a[1],"w")) == NULL)
+	if ((f=fopen(a[1],"w")) == NULL)
 		return -1;
 	signal(SIGINT,end);
 	char c;
 	while(1){
-		if ((c = getchar()) != -1) {
+		if ((c=getchar())!=-1){
 			fprintf(f,"%c",c);
 		}
 	}
