82aadbab6f4ae34bd9790613f631d3a7dcbcd1e7
Shreyas <skraman1999@gmail.com>
2014-03-05 19:56:38 -0800

    Made it readable.

--- yate.c
+++ yate.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+
 FILE *f;
-void end(int signal){
+
+void end(int signal)
+{
 	fclose(f);
 	exit(0);
 }
-int main(int n,char *a[]){
-	if (n!=2)
+
+int main(int n, char *a[])
+{
+	if (n != 2)
+	{
 		return -1;
-	if ((f=fopen(a[1],"w"))==NULL)
+	}
+	
+	if ((f=fopen(a[1],"w")) == NULL)
+	{
 		return -1;
-	signal(SIGINT,end);
+	}
+	
+	signal(SIGINT, end);
+	
 	char c;
-	while(1){
-		if ((c=getchar())!=-1){
-			fprintf(f,"%c",c);
+	while(1)
+	{
+		if ((c=getchar()) != -1)
+		{
+			fprintf(f, "%c", c);
 		}
 	}
 }
