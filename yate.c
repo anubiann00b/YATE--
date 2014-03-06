749d2a6de2b5414dc83842802ba4d587c0c3f360
Shreyas <skraman1999@gmail.com>
2014-03-05 17:29:14 -0800

    This was wasting precious free space on my hard drive... 274 bytes and counting.

--- yate.c
+++ yate.c
@@ -1,21 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-FILE *f;
-void end(int signal){
-	fclose(f);
-	exit(0);
-}
-int main(int n,char *a[]){
-	if (n!=2)
-		return -1;
-	if ((f=fopen(a[1],"w"))==NULL)
-		return -1;
-	signal(SIGINT,end);
-	char c;
-	while(1){
-		if ((c=getchar())!=-1){
-			fprintf(f,"%c",c);
-		}
-	}
-}
+FILE *f;void end(int signal){fclose(f);exit(0);}int main(int n,char *a[]){if (n!=2)return -1;if ((f=fopen(a[1],"w"))==NULL)return -1;signal(SIGINT,end);char c;while(1){if ((c=getchar())!=-1){fprintf(f,"%c",c);}}}
