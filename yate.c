1e39a7f247d8af8ae92bd99cc6568d4d272cfcbb
Shreyas <skraman1999@gmail.com>
2014-03-05 17:05:03 -0800

    Shortened variable names.

--- yate.c
+++ yate.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-FILE *fp;
+FILE *f;
 void end(int signal){
-	fclose(fp);
+	fclose(f);
 	exit(0);
 }
-int main(int argc, char *argv[]){
-	if (argc != 2)
+int main(int n, char *a[]){
+	if (n != 2)
 		return -1;
-	if ((fp = fopen(argv[1],"w")) == NULL)
+	if ((f = fopen(a[1],"w")) == NULL)
 		return -1;
 	signal(SIGINT,end);
 	char c;
 	while(1){
 		if ((c = getchar()) != -1) {
-			fprintf(fp,"%c",c);
+			fprintf(f,"%c",c);
 		}
 	}
 }
